68d3a6d15041390306cf0e3b5656655ff2f8aac4
jayLrei <thfpdhk1@gmail.com>
2025-12-28 11:42:52 +0900

    [Chore]

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   const navigate = useNavigate();
-
-  const handleClick = () => {
-    if (count === 1) {
-      navigate("/game");
-    } else {
-      setCount(count + 1);
-    }
-  };
 
   return (
     <div className="w-full min-h-screen flex flex-col items-center justify-center relative">
@@ -20,12 +12,12 @@ export default function Home() {
       </div>
       <div
         className="font-bold p-2 text-[40px] p-8 rounded-lg hover:bg-gradient-to-r from-orange-500 to-cyan-500 transition-all duraion-700 hover:text-white cursor-pointer"
-        onClick={handleClick}
+        onClick={() => navigate("/game")}
       >
         신나는 주루마블 START
       </div>
 
-      {count === 1 && (
+      {/* {count === 1 && (
         <div className="fixed flex items-center justify-center z-10">
           <img
             src="/images/bonobono.jpg"
@@ -37,7 +29,7 @@ export default function Home() {
             }}
           />
         </div>
-      )}
+      )} */}
     </div>
   );
 }
